121ecd5ccbc87c4cc350fb2bb99fbba71b9fff55
jhojin8380-925 <jhojin8380@gmail.com>
2026-05-19 16:44:00 +0900

    깃 적용테스트2

--- test/src/b_task/Main.java
+++ test/src/b_task/Main.java
@@ -4,9 +4,19 @@ public class Main {
 	
 	public static void main(String[] args) {
 		StudentMain student = new StudentMain("홍길동", 19, 70, 60, 90);
+		StudentMain student2 = new StudentMain("짱구", 5, 50, 70, 100);
+		StudentMain student3 = new StudentMain("철수", 6, 90, 70, 40);
 		System.out.println("===== 학생 정보 =====");
 		student.printInfo();
 		student.getTotal();
 		student.getAvg();
+
+		student2.printInfo();
+		student2.getTotal();
+		student2.getAvg();
+		
+		student3.printInfo();
+		student3.getTotal();
+		student3.getAvg();
 	}
 }
